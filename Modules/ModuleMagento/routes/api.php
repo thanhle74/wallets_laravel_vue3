09cc7c1f4995a56350f960f79ae108dd846932c3
thanhle74 <160691540+thanhle74@users.noreply.github.com>
2025-03-20 23:48:28 +0700

    1.0.12

--- Modules/ModuleMagento/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\ModuleMagento\Http\Controllers\ModuleMagentoController;
-
-Route::post('/modulemagento', [ModuleMagentoController::class, 'generate']);
-// Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-//     Route::post('/modulemagento', [ModuleMagentoController::class, 'generate']);
-// });
